ea87125cdb32093e540652fac941276591b7f226
Sreynoch3737 <149126076+Sreynoch3737@users.noreply.github.com>
2023-11-17 09:39:44 +0700

    upload Auth

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::get("Example", function(){
 
     return view( 'test_route', $data);
  });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
